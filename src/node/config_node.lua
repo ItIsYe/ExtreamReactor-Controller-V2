3ac3e49320be09155e6bf66dd598edb9d2be1580
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2025-09-06 02:09:38 +0200

    Update config_node.lua

--- src/node/config_node.lua
+++ src/node/config_node.lua
@@ -1,1 +1,22 @@
-return {modem_side="right",auth_token="changeme",floor=1}
+return {
+modem_side = "right",
+auth_token = "changeme", -- Master & Nodes müssen identisch sein
+node_id = nil,
+floor = 1,
+rpm_target_default = 1800,
+flow_step = 50,
+flow_min = 0,
+flow_max = 25000,
+rod_step = 1,
+rod_min = 0,
+rod_max = 100,
+reactor = nil,
+turbines = {},
+floor_storages = {},
+
+
+-- Alarme lokal
+alarm_rpm_low = 1600,
+alarm_rpm_high = 1950,
+alarm_enable_sound = false,
+}
