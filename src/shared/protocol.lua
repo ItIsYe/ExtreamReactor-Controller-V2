1eff96ed7f9c7ea4082ebee2a0bd1e7f724776be
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2025-09-06 02:08:46 +0200

    Update protocol.lua

--- src/shared/protocol.lua
+++ src/shared/protocol.lua
@@ -1,1 +1,31 @@
-return { PROTO="xreactor.net.v2" }
+local M = {}
+M.PROTO = "xreactor.net.v2"
+
+
+local function has_json() return textutils.serializeJSON and textutils.unserializeJSON end
+
+
+function M.send(id, tbl, token)
+if not has_json() then error("JSON not available") end
+tbl._auth = token
+rednet.send(id, textutils.serializeJSON(tbl), M.PROTO)
+end
+
+
+function M.broadcast(tbl, token)
+if not has_json() then error("JSON not available") end
+tbl._auth = token
+rednet.broadcast(textutils.serializeJSON(tbl), M.PROTO)
+end
+
+
+function M.recv(timeout)
+local id, msg, proto = rednet.receive(M.PROTO, timeout)
+if not id then return nil end
+local ok, data = pcall(textutils.unserializeJSON, msg)
+if not ok then return nil end
+return id, data, proto
+end
+
+
+return M
