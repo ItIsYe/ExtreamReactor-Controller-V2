c8c7a34a5f7755fb70024e5459e27d8e633de61a
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2025-10-31 22:50:06 +0100

    Update config_identity.lua

--- xreactor/config_identity.lua
+++ xreactor/config_identity.lua
@@ -1,1 +1,12 @@
--- config_identity.lua (Final)
+--========================================================
+-- /xreactor/config_identity.lua
+-- Identität dieses Computers im XReactor-Cluster
+--========================================================
+return {
+  role     = "MASTER",            -- MASTER | REACTOR | FUEL | WASTE | AUX
+  id       = "01",
+  hostname = "",                  -- leer => auto "XR-MASTER-01"
+  cluster  = "XR-CLUSTER-ALPHA",
+  token    = "xreactor"
+}
+
