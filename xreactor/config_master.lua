5039d47759f013c5221d177650485ae76f94707a
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2025-10-31 23:07:22 +0100

    Create config_master.lua

---
+++ xreactor/config_master.lua
@@ -0,0 +1,6 @@
+-- /xreactor/config_master.lua
+return {
+  modem_side   = nil,   -- z.B. "right"; nil = auto (Launcher öffnet alle Modems)
+  monitor_side = nil,   -- z.B. "top"; nil = auto (erster gefundener Monitor)
+  text_scale   = 0.5,   -- 0.5 .. 5.0
+}
